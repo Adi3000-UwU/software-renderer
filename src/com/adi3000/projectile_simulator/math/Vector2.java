9caa579615b38b03dbb9ee16c5287e2d5436bafa
Adi3000 <adam.jan.gryglas@gmail.com>
2025-02-19 13:35:55 +0100

    feat: Add Vector2 and Vector3 types

---
+++ src/com/adi3000/projectile_simulator/math/Vector2.java
@@ -0,0 +1,143 @@
+package com.adi3000.projectile_simulator.math;
+
+@SuppressWarnings("unused")
+public class Vector2 {
+    
+    private double x;
+    private double y;
+    
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Vector2(Vector2 v) {
+        this(v.x, v.y);
+    }
+    public Vector2(Vector3 v) {
+        this(v.getX(), v.getY());
+    }
+    public Vector2(double[] components) {
+        this(components[0], components[1]);
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector2 vector2 = (Vector2) o;
+        return x == vector2.x && y == vector2.y;
+    }
+    @Override
+    public String toString() {
+        return "Vector2{" + "x=" + x + ", y=" + y + '}';
+    }
+    public double[] toArray() {
+        return new double[] {x, y};
+    }
+    public Vector3 toVector3() {
+        return new Vector3(x, y, 0);
+    }
+    
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    
+    public void set(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void set(Vector2 v) {
+        set(v.x, v.y);
+    }
+    public void set(Vector3 v) {
+        set(v.getX(), v.getY());
+    }
+    public void set(double[] components) {
+        set(components[0], components[1]);
+    }
+    public void setMagnitude(double mag) {
+        set(getNormilized().mult(mag));
+    }
+    
+    public Vector2 add(Vector2 v) {
+        return add(this, v);
+    }
+    public static Vector2 add(Vector2 v1, Vector2 v2) {
+        return new Vector2(v1.x + v2.x, v1.y + v2.y);
+    }
+    public Vector2 sub(Vector2 v) {
+        return sub(this, v);
+    }
+    public static Vector2 sub(Vector2 v1, Vector2 v2) {
+        return new Vector2(v1.x - v2.x, v1.y - v2.y);
+    }
+    public Vector2 mult(double num) {
+        return mult(this, num);
+    }
+    public static Vector2 mult(Vector2 v, double num) {
+        return new Vector2(v.x * num, v.y * num);
+    }
+    public Vector2 div(double num) {
+        return mult(this, num);
+    }
+    public static Vector2 div(Vector2 v, double num) {
+        return new Vector2(v.x / num, v.y / num);
+    }
+    
+    public double mag() {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+    public Vector2 normilize() {
+        set(getNormilized());
+        return this;
+    }
+    public Vector2 getNormilized() {
+        double mag_inv = 1.0 / mag();
+        return new Vector2(x * mag_inv, y * mag_inv);
+    }
+    
+    public double dot(Vector2 v) {
+        return dot(this, v);
+    }
+    public static double dot(Vector2 v1, Vector2 v2) {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+    public Vector3 cross(Vector2 v) {
+        return cross(this, v);
+    }
+    public static Vector3 cross(Vector2 v1, Vector2 v2) {
+        return Vector3.cross(v1.toVector3(), v2.toVector3());
+    }
+    
+    public double distance(Vector2 v) {
+        return distance(this, v);
+    }
+    public static double distance(Vector2 v1, Vector2 v2) {
+        return Math.sqrt(Math.pow(v2.x - v1.x, 2) + Math.pow(v2.y - v1.y, 2));
+    }
+    
+    public double angle(Vector2 v) {
+        return angle(this, v);
+    }
+    public static double angle(Vector2 v1, Vector2 v2) {
+        return Math.acos(v1.dot(v2) / (v1.mag() * v2.mag()));
+    }
+    public double heading() {
+        return angle(new Vector2(1, 0));
+    }
+    public Vector2 rotate(double angle) {
+        double tempX = Math.cos(angle) * x - Math.sin(angle) * y;
+        double tempY = Math.sin(angle) * x + Math.cos(angle) * y;
+        set(tempX, tempY);
+        return this;
+    }
+    
+}
---
+++ src/com/adi3000/projectile_simulator/math/Vector3.java
@@ -0,0 +1,141 @@
+package com.adi3000.projectile_simulator.math;
+
+@SuppressWarnings("unused")
+public class Vector3 {
+    
+    private double x;
+    private double y;
+    private double z;
+    
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public Vector3(Vector3 v) {
+        this(v.x, v.y, v.z);
+    }
+    public Vector3(Vector2 v) {
+        this(v.getX(), v.getY(), 0);
+    }
+    public Vector3(double[] components) {
+        this(components[0], components[1], components[2]);
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector3 vector3 = (Vector3) o;
+        return Double.compare(x, vector3.x) == 0 && Double.compare(y, vector3.y) == 0 && Double.compare(z, vector3.z) == 0;
+    }
+    @Override
+    public String toString() {
+        return "Vector3{" + "x=" + x + ", y=" + y + ", z=" + z + '}';
+    }
+    public double[] toArray() {
+        return new double[] {x, y, z};
+    }
+    public Vector2 toVector2() {
+        return new Vector2(x, y);
+    }
+    
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double getZ() {
+        return z;
+    }
+    public void setZ(double z) {
+        this.z = z;
+    }
+    
+    public void set(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public void set(Vector3 v) {
+        set(v.x, v.y, v.z);
+    }
+    public void set(Vector2 v) {
+        set(v.getX(), v.getY(), 0);
+    }
+    public void set(double[] components) {
+        set(components[0], components[1], components[2]);
+    }
+    public void setMagnitude(double mag) {
+        set(getNormilized().mult(mag));
+    }
+    
+    public Vector3 add(Vector3 v) {
+        return add(this, v);
+    }
+    public static Vector3 add(Vector3 v1, Vector3 v2) {
+        return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+    }
+    public Vector3 sub(Vector3 v) {
+        return sub(this, v);
+    }
+    public static Vector3 sub(Vector3 v1, Vector3 v2) {
+        return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+    }
+    public Vector3 mult(double num) {
+        return mult(this, num);
+    }
+    public static Vector3 mult(Vector3 v, double num) {
+        return new Vector3(v.x * num, v.y * num, v.z * num);
+    }
+    public Vector3 div(double num) {
+        return mult(this, num);
+    }
+    public static Vector3 div(Vector3 v, double num) {
+        return new Vector3(v.x / num, v.y / num, v.z / num);
+    }
+    
+    public double mag() {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+    }
+    public Vector3 normilize() {
+        set(getNormilized());
+        return this;
+    }
+    public Vector3 getNormilized() {
+        double mag_inv = 1.0 / mag();
+        return new Vector3(x * mag_inv, y * mag_inv, z * mag_inv);
+    }
+    
+    public double dot(Vector3 v) {
+        return dot(this, v);
+    }
+    public static double dot(Vector3 v1, Vector3 v2) {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+    public Vector3 cross(Vector3 v) {
+        return cross(this, v);
+    }
+    public static Vector3 cross(Vector3 v1, Vector3 v2) {
+        double x = v1.y * v2.z - v1.z * v2.y;
+        double y = v1.z * v2.x - v1.x * v2.z;
+        double z = v1.x * v2.y - v1.y * v2.x;
+        
+        return new Vector3(x, y, z);
+    }
+    
+    public double distance(Vector3 v) {
+        return distance(this, v);
+    }
+    public static double distance(Vector3 v1, Vector3 v2) {
+        return Math.sqrt(Math.pow(v2.x - v1.x, 2) + Math.pow(v2.y - v1.y, 2) + Math.pow(v2.z - v1.z, 2));
+    }
+    
+    // TODO Add angle calculations using quaternions
+}
